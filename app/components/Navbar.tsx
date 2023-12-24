753d878aa0d003b738e5abe326bee1ae990663da
Cuong Nguyen Huy <cuongnguyen@remitano.com>
2023-12-24 10:49:28 +0700

    refactor current ui of dashboard page

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,70 +1,70 @@
 'use client';
 
 import Logo from '@img/logos/logo.png';
-import { useClusterPath } from '@utils/url';
+import {useClusterPath} from '@utils/url';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useSelectedLayoutSegment, useSelectedLayoutSegments } from 'next/navigation';
+import {useSelectedLayoutSegment, useSelectedLayoutSegments} from 'next/navigation';
 import React from 'react';
 
-import { ClusterStatusButton } from './ClusterStatusButton';
+import {ClusterStatusButton} from './ClusterStatusButton';
 
 export function Navbar() {
-    // TODO: use `collapsing` to animate collapsible navbar
-    const [collapse, setCollapse] = React.useState(false);
-    const homePath = useClusterPath({ pathname: '/' });
-    const supplyPath = useClusterPath({ pathname: '/supply' });
-    const inspectorPath = useClusterPath({ pathname: '/tx/inspector' });
-    const selectedLayoutSegment = useSelectedLayoutSegment();
-    const selectedLayoutSegments = useSelectedLayoutSegments();
-    return (
-        <nav className="navbar navbar-expand-md navbar-light">
-            <div className="container">
-                <Link href={homePath}>
-                    <Image alt="RENEC Explorer" height={28} src={Logo} />
-                </Link>
+  // TODO: use `collapsing` to animate collapsible navbar
+  const [collapse, setCollapse] = React.useState(false);
+  const homePath = useClusterPath({pathname: '/'});
+  const supplyPath = useClusterPath({pathname: '/supply'});
+  const inspectorPath = useClusterPath({pathname: '/tx/inspector'});
+  const selectedLayoutSegment = useSelectedLayoutSegment();
+  const selectedLayoutSegments = useSelectedLayoutSegments();
+  return (
+    <nav className="navbar navbar-expand-md navbar-light">
+      <div className="container">
+        <Link href={homePath}>
+          <Image alt="RENEC Explorer" height={28} src={Logo}/>
+        </Link>
 
-                <button className="navbar-toggler" type="button" onClick={() => setCollapse(value => !value)}>
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+        <button className="navbar-toggler" type="button" onClick={() => setCollapse(value => !value)}>
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-                <div className={`collapse navbar-collapse ms-auto me-4 ${collapse ? 'show' : ''}`}>
-                    <ul className="navbar-nav me-auto">
-                        <li className="nav-item">
-                            <Link
-                                className={`nav-link${selectedLayoutSegment === null ? ' active' : ''}`}
-                                href={homePath}
-                            >
-                                Cluster Stats
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link
-                                className={`nav-link${selectedLayoutSegment === 'supply' ? ' active' : ''}`}
-                                href={supplyPath}
-                            >
-                                Supply
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link
-                                className={`nav-link${
-                                    selectedLayoutSegments[0] === 'tx' && selectedLayoutSegments[1] === '(inspector)'
-                                        ? ' active'
-                                        : ''
-                                }`}
-                                href={inspectorPath}
-                            >
-                                Inspector
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
+        <div className={`collapse navbar-collapse ms-auto me-4 ${collapse ? 'show' : ''}`}>
+          <ul className="navbar-nav me-auto">
+            <li className="nav-item">
+              <Link
+                className={`nav-link${selectedLayoutSegment === null ? ' active' : ''}`}
+                href={homePath}
+              >
+                Dashboard
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className={`nav-link${selectedLayoutSegment === 'supply' ? ' active' : ''}`}
+                href={supplyPath}
+              >
+                Supply
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className={`nav-link${
+                  selectedLayoutSegments[0] === 'tx' && selectedLayoutSegments[1] === '(inspector)'
+                    ? ' active'
+                    : ''
+                }`}
+                href={inspectorPath}
+              >
+                Inspector
+              </Link>
+            </li>
+          </ul>
+        </div>
 
-                <div className="d-none d-md-block">
-                    <ClusterStatusButton />
-                </div>
-            </div>
-        </nav>
-    );
+        <div className="d-none d-md-block">
+          <ClusterStatusButton/>
+        </div>
+      </div>
+    </nav>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,211 +1,194 @@
 'use client';
 
-import { Epoch } from '@components/common/Epoch';
-import { ErrorCard } from '@components/common/ErrorCard';
-import { LoadingCard } from '@components/common/LoadingCard';
-import { Slot } from '@components/common/Slot';
-import { TableCardBody } from '@components/common/TableCardBody';
-import { TimestampToggle } from '@components/common/TimestampToggle';
-import { LiveTransactionStatsCard } from '@components/LiveTransactionStatsCard';
-import { StatsNotReady } from '@components/StatsNotReady';
-import { useVoteAccounts } from '@providers/accounts/vote-accounts';
-import { useCluster } from '@providers/cluster';
-import { StatsProvider } from '@providers/stats';
+import {ErrorCard} from '@components/common/ErrorCard';
+import {LoadingCard} from '@components/common/LoadingCard';
+import {LiveTransactionStatsCard} from '@components/LiveTransactionStatsCard';
+import {StatsNotReady} from '@components/StatsNotReady';
+import {useVoteAccounts} from '@providers/accounts/vote-accounts';
+import {useCluster} from '@providers/cluster';
+import {StatsProvider} from '@providers/stats';
 import {
-    ClusterStatsStatus,
-    useDashboardInfo,
-    usePerformanceInfo,
-    useStatsProvider,
+  ClusterStatsStatus,
+  useDashboardInfo,
+  usePerformanceInfo,
+  useStatsProvider,
 } from '@providers/stats/solanaClusterStats';
-import { Status, SupplyProvider, useFetchSupply, useSupply } from '@providers/supply';
-import { ClusterStatus } from '@utils/cluster';
-import { abbreviatedNumber, lamportsToSol, slotsToHumanString } from '@utils/index';
-import { percentage } from '@utils/math';
+import {Status, SupplyProvider, useFetchSupply, useSupply} from '@providers/supply';
+import {ClusterStatus} from '@utils/cluster';
+import {abbreviatedNumber, lamportsToSol, slotsToHumanString} from '@utils/index';
+import {percentage} from '@utils/math';
 import React from 'react';
 
 export default function Page() {
-    return (
-        <StatsProvider>
-            <SupplyProvider>
-                <div className="container mt-4">
-                    <StakingComponent />
-                    <div className="card">
-                        <div className="card-header">
-                            <div className="row align-items-center">
-                                <div className="col">
-                                    <h4 className="card-header-title">Live Cluster Stats</h4>
-                                </div>
-                            </div>
-                        </div>
-                        <StatsCardBody />
-                    </div>
-                    <LiveTransactionStatsCard />
-                </div>
-            </SupplyProvider>
-        </StatsProvider>
-    );
+  return (
+    <StatsProvider>
+      <SupplyProvider>
+        <div className="container mt-4">
+          <StatsCardBody/>
+          <LiveTransactionStatsCard/>
+          <StakingComponent/>
+        </div>
+      </SupplyProvider>
+    </StatsProvider>
+  );
 }
 
 function StakingComponent() {
-    const { status } = useCluster();
-    const supply = useSupply();
-    const fetchSupply = useFetchSupply();
-    const { fetchVoteAccounts, voteAccounts } = useVoteAccounts();
+  const {status} = useCluster();
+  const supply = useSupply();
+  const fetchSupply = useFetchSupply();
+  const {fetchVoteAccounts, voteAccounts} = useVoteAccounts();
 
-    function fetchData() {
-        fetchSupply();
-        fetchVoteAccounts();
+  function fetchData() {
+    fetchSupply();
+    fetchVoteAccounts();
+  }
+
+  React.useEffect(() => {
+    if (status === ClusterStatus.Connected) {
+      fetchData();
     }
+  }, [status]); // eslint-disable-line react-hooks/exhaustive-deps
 
-    React.useEffect(() => {
-        if (status === ClusterStatus.Connected) {
-            fetchData();
-        }
-    }, [status]); // eslint-disable-line react-hooks/exhaustive-deps
-
-    const delinquentStake = React.useMemo(() => {
-        if (voteAccounts) {
-            return voteAccounts.delinquent.reduce((prev, current) => prev + current.activatedStake, BigInt(0));
-        }
-    }, [voteAccounts]);
-
-    const activeStake = React.useMemo(() => {
-        if (voteAccounts && delinquentStake) {
-            return voteAccounts.current.reduce((prev, current) => prev + current.activatedStake, BigInt(0)) + delinquentStake;
-        }
-    }, [voteAccounts, delinquentStake]);
-
-    if (supply === Status.Disconnected) {
-        // we'll return here to prevent flicker
-        return null;
+  const delinquentStake = React.useMemo(() => {
+    if (voteAccounts) {
+      return voteAccounts.delinquent.reduce((prev, current) => prev + current.activatedStake, BigInt(0));
     }
+  }, [voteAccounts]);
 
-    if (supply === Status.Idle || supply === Status.Connecting) {
-        return <LoadingCard message="Loading supply data" />;
-    } else if (typeof supply === 'string') {
-        return <ErrorCard text={supply} retry={fetchData} />;
+  const activeStake = React.useMemo(() => {
+    if (voteAccounts && delinquentStake) {
+      return voteAccounts.current.reduce((prev, current) => prev + current.activatedStake, BigInt(0)) + delinquentStake;
     }
+  }, [voteAccounts, delinquentStake]);
 
-    // Calculate to 2dp for accuracy, then display as 1
-    const circulatingPercentage = percentage(supply.circulating, supply.total, 2).toFixed(1);
+  if (supply === Status.Disconnected) {
+    // we'll return here to prevent flicker
+    return null;
+  }
 
-    let delinquentStakePercentage;
-    if (delinquentStake && activeStake) {
-        delinquentStakePercentage = percentage(delinquentStake, activeStake, 2).toFixed(1);
-    }
+  if (supply === Status.Idle || supply === Status.Connecting) {
+    return <LoadingCard message="Loading supply data"/>;
+  } else if (typeof supply === 'string') {
+    return <ErrorCard text={supply} retry={fetchData}/>;
+  }
 
-    return (
-        <div className="row staking-card">
-            <div className="col-6 col-xl">
-                <div className="card">
-                    <div className="card-body">
-                        <h4>Circulating Supply</h4>
-                        <h1>
-                            <em>{displayLamports(supply.circulating)}</em> /{' '}
-                            <small>{displayLamports(supply.total)}</small>
-                        </h1>
-                        <h5>
-                            <em>{circulatingPercentage}%</em> is circulating
-                        </h5>
-                    </div>
-                </div>
-            </div>
-            <div className="col-6 col-xl">
-                <div className="card">
-                    <div className="card-body">
-                        <h4>Active Stake</h4>
-                        {activeStake ? (
-                            <h1>
-                                <em>{displayLamports(activeStake)}</em> / <small>{displayLamports(supply.total)}</small>
-                            </h1>
-                        ) : null}
-                        {delinquentStakePercentage && (
-                            <h5>
-                                Delinquent stake: <em>{delinquentStakePercentage}%</em>
-                            </h5>
-                        )}
-                    </div>
-                </div>
-            </div>
+  // Calculate to 2dp for accuracy, then display as 1
+  const circulatingPercentage = percentage(supply.circulating, supply.total, 2).toFixed(1);
+
+  let delinquentStakePercentage;
+  if (delinquentStake && activeStake) {
+    delinquentStakePercentage = percentage(delinquentStake, activeStake, 2).toFixed(1);
+  }
+
+  return (
+    <div className="row staking-card">
+      <div className="col-12 col-xl-6">
+        <div className="card">
+          <div className="card-body">
+            <h4>Active Stake</h4>
+            {activeStake ? (
+              <h1>
+                <em>{displayLamports(activeStake)}</em> / <small>{displayLamports(supply.total)}</small>
+              </h1>
+            ) : null}
+            {delinquentStakePercentage && (
+              <h5>
+                Delinquent stake: <em>{delinquentStakePercentage}%</em>
+              </h5>
+            )}
+          </div>
         </div>
-    );
+      </div>
+      <div className="col-12 col-xl-6">
+        <div className="card">
+          <div className="card-body">
+            <h4>Circulating Supply</h4>
+            <h1>
+              <em>{displayLamports(supply.circulating)}</em> /{' '}
+              <small>{displayLamports(supply.total)}</small>
+            </h1>
+            <h5>
+              <em>{circulatingPercentage}%</em> is circulating
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 function displayLamports(value: number | bigint) {
-    return abbreviatedNumber(lamportsToSol(value));
+  return abbreviatedNumber(lamportsToSol(value));
 }
 
 function StatsCardBody() {
-    const dashboardInfo = useDashboardInfo();
-    const performanceInfo = usePerformanceInfo();
-    const { setActive } = useStatsProvider();
-    const { cluster } = useCluster();
+  const dashboardInfo = useDashboardInfo();
+  const performanceInfo = usePerformanceInfo();
+  const {setActive} = useStatsProvider();
+  const {cluster} = useCluster();
 
-    React.useEffect(() => {
-        setActive(true);
-        return () => setActive(false);
-    }, [setActive, cluster]);
+  React.useEffect(() => {
+    setActive(true);
+    return () => setActive(false);
+  }, [setActive, cluster]);
 
-    if (performanceInfo.status !== ClusterStatsStatus.Ready || dashboardInfo.status !== ClusterStatsStatus.Ready) {
-        const error =
-            performanceInfo.status === ClusterStatsStatus.Error || dashboardInfo.status === ClusterStatsStatus.Error;
-        return <StatsNotReady error={error} />;
-    }
+  if (performanceInfo.status === ClusterStatsStatus.Loading) {
+    return <LoadingCard message="Loading epoch data"/>
+  }
 
-    const { avgSlotTime_1h, avgSlotTime_1min, epochInfo, blockTime } = dashboardInfo;
-    const hourlySlotTime = Math.round(1000 * avgSlotTime_1h);
-    const averageSlotTime = Math.round(1000 * avgSlotTime_1min);
-    const { slotIndex, slotsInEpoch } = epochInfo;
-    const epochProgress = percentage(slotIndex, slotsInEpoch, 2).toFixed(1) + '%';
-    const epochTimeRemaining = slotsToHumanString(Number(slotsInEpoch - slotIndex), hourlySlotTime);
-    const { blockHeight, absoluteSlot } = epochInfo;
+  if (performanceInfo.status !== ClusterStatsStatus.Ready || dashboardInfo.status !== ClusterStatsStatus.Ready) {
+    const error =
+      performanceInfo.status === ClusterStatsStatus.Error || dashboardInfo.status === ClusterStatsStatus.Error;
+    return <StatsNotReady error={error}/>;
+  }
 
-    return (
-        <TableCardBody>
-            <tr>
-                <td className="w-100">Slot</td>
-                <td className="text-lg-end font-monospace">
-                    <Slot slot={absoluteSlot} link />
-                </td>
-            </tr>
-            {blockHeight !== undefined && (
-                <tr>
-                    <td className="w-100">Block height</td>
-                    <td className="text-lg-end font-monospace">
-                        <Slot slot={blockHeight} />
-                    </td>
-                </tr>
-            )}
-            {blockTime && (
-                <tr>
-                    <td className="w-100">Cluster time</td>
-                    <td className="text-lg-end font-monospace">
-                        <TimestampToggle unixTimestamp={blockTime}></TimestampToggle>
-                    </td>
-                </tr>
-            )}
-            <tr>
-                <td className="w-100">Slot time (1min average)</td>
-                <td className="text-lg-end font-monospace">{averageSlotTime}ms</td>
-            </tr>
-            <tr>
-                <td className="w-100">Slot time (1hr average)</td>
-                <td className="text-lg-end font-monospace">{hourlySlotTime}ms</td>
-            </tr>
-            <tr>
-                <td className="w-100">Epoch</td>
-                <td className="text-lg-end font-monospace">
-                    <Epoch epoch={epochInfo.epoch} link />
-                </td>
-            </tr>
-            <tr>
-                <td className="w-100">Epoch progress</td>
-                <td className="text-lg-end font-monospace">{epochProgress}</td>
-            </tr>
-            <tr>
-                <td className="w-100">Epoch time remaining (approx.)</td>
-                <td className="text-lg-end font-monospace">~{epochTimeRemaining}</td>
-            </tr>
-        </TableCardBody>
-    );
+  const {avgSlotTime_1h, avgSlotTime_1min, epochInfo} = dashboardInfo;
+  const hourlySlotTime = Math.round(1000 * avgSlotTime_1h);
+  const averageSlotTime = Math.round(1000 * avgSlotTime_1min);
+  const {slotIndex, slotsInEpoch} = epochInfo;
+  const epochProgress = percentage(slotIndex, slotsInEpoch, 2).toFixed(1) + '%';
+  const epochTimeRemaining = slotsToHumanString(Number(slotsInEpoch - slotIndex), hourlySlotTime);
+  const {absoluteSlot} = epochInfo;
+
+  return (
+    <div className="card epoch-stats">
+      <div className="row card-body">
+        <div className="col-12 col-xl-3">
+          <div className="mb-2 mb-xl-3">Slot height</div>
+          <h1 className="text-primary pb-0 mb-0">
+            {absoluteSlot.toLocaleString()}
+          </h1>
+        </div>
+        <div className="col-12 col-xl-3 mt-4 mt-xl-0">
+          <div className="mb-2 mb-xl-3">Current slot time</div>
+          <h1 className="text-primary pb-0 mb-0">
+            {averageSlotTime} ms
+          </h1>
+        </div>
+        <div className="col-12 col-xl-6 mt-4 mt-xl-0">
+          <div className="mb-2 mb-xl-3">Epoch</div>
+          <div className="d-flex align-items-center">
+            <h1 className="text-primary pb-0 mb-0">{epochInfo.epoch.toLocaleString('en-US')}</h1>
+            <div className="flex-grow-1 mx-5">
+              <div className="w-100 d-flex justify-content-between mb-2">
+                <span>{epochProgress}</span>
+                <span>ETA: {epochTimeRemaining}</span>
+              </div>
+              <div className="progress">
+                <div
+                  className="progress-bar"
+                  role="progressbar"
+                  aria-valuemin={0}
+                  aria-valuemax={100}
+                  style={{width: epochProgress}}
+                />
+              </div>
+            </div>
+            <h1 className="pb-0 mb-0">{Number(epochInfo.epoch.toLocaleString()) + 1}</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
