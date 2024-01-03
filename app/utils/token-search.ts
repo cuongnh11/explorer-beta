cf6a16a65166a4326a70c6c78a24f280d1060127
dungtrinh97 <dungtrinh@remitano.com>
2024-01-03 23:13:50 +0700

    Handle search tokens

--- app/utils/token-search.ts
+++ app/utils/token-search.ts
@@ -1,30 +1,6 @@
-/**
- * We use a third-party API (https://token-list-api.solana.cloud)
- * See https://github.com/solflare-wallet/utl-api
- * Note that the utl-sdk (https://github.com/solflare-wallet/utl-sdk/tree/master)
- * doesn't use a fallback for search
- * So to avoid pulling in extra dependencies we just use the public API directly for search
- */
-
-import { Base58EncodedAddress } from 'web3js-experimental';
+import { TokenListProvider } from '@renec-foundation/rpl-token-registry';
 
 import { Cluster } from './cluster';
-
-type TokenSearchApiResponseToken = {
-    address: Base58EncodedAddress;
-    chainId: number;
-    name: string;
-    symbol: string;
-    verified: boolean;
-    decimals: number;
-    holders: number;
-    logoUri: string;
-    tags: string[];
-};
-
-type TokenSearchApiResponse = {
-    content: TokenSearchApiResponseToken[];
-};
 
 type SearchElement = {
     label: string;
@@ -45,28 +21,14 @@ export async function searchTokens(search: string, cluster: Cluster): Promise<Se
         return [];
     }
 
-    const apiResponse = await fetch(
-        `https://token-list-api.solana.cloud/v1/search?query=${encodeURIComponent(
-            search
-        )}&chainId=${chainId}&start=0&limit=20`
-    );
-    if (apiResponse.status >= 400) {
-        try {
-            const errorJsonBody = await apiResponse.json();
-            console.error(new Error('Error calling token search API'), {
-                chainId: chainId.toString(),
-                errorJsonBody,
-                search,
-            });
-        } catch {
-            // no JSON body for error
-            console.error(new Error('Error calling token search API'), { chainId: chainId.toString(), search });
-        }
-    }
+    const tokens = await new TokenListProvider().resolve();
+    const tokenList = tokens.filterByChainId(chainId).getList();
 
-    const { content } = (await apiResponse.json()) as TokenSearchApiResponse;
-
-    return content.map(token => ({
+    const searchValue = search.trim().toLocaleLowerCase();
+    return tokenList.filter(item => {
+        const searchFields = ['name', 'symbol', 'address'];
+        return searchFields.some(field => (item as any)[field].toLocaleLowerCase().includes(searchValue) )
+    }).map(token => ({
         label: token.name,
         pathname: '/address/' + token.address,
         value: [token.name, token.symbol, token.address],
