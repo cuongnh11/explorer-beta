96801e417f86ef119d33398a854f8e20ad2701a8
Cuong Nguyen Huy <cuongnguyen@remitano.com>
2024-01-05 13:35:35 +0700

    init blocks page

---
+++ app/blocks/page-client.tsx
@@ -0,0 +1,12 @@
+"use client"
+const BlockPageClient = () => {
+    return (
+        <div className="mt-4 mb-4 transaction-card">
+            <div className="mb-3 cursor-pointer">
+                Recent blocks <i className="fe fe-repeat text-primary" />
+            </div>
+        </div>
+    );
+};
+
+export default BlockPageClient
