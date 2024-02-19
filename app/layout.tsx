94ce236adb0592c7480dfbd7926af2e71346f420
Pham Anh Tuan <tuanpham@remitano.com>
2024-02-19 16:42:50 +0700

    [renec] setup multiple language for explore beta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,17 @@
 import './scss/theme-dark.scss';
 
-import {ClusterModal} from '@components/ClusterModal';
-import {ClusterStatusBanner} from '@components/ClusterStatusButton';
-import Footer from "@components/Footer";
-import {MessageBanner} from '@components/MessageBanner';
-import {Navbar} from '@components/Navbar';
-import {SearchBar} from '@components/SearchBar';
-import {ClusterProvider} from '@providers/cluster';
-import {ScrollAnchorProvider} from '@providers/scroll-anchor';
-import {Inter} from 'next/font/google';
-import {Metadata} from 'next/types';
-import React from 'react'
+import { ClusterModal } from '@components/ClusterModal';
+import { ClusterStatusBanner } from '@components/ClusterStatusButton';
+import Footer from '@components/Footer';
+import { MessageBanner } from '@components/MessageBanner';
+import { Navbar } from '@components/Navbar';
+import { SearchBar } from '@components/SearchBar';
+import { ClusterProvider } from '@providers/cluster';
+import { LanguageProvider } from '@providers/language-provider';
+import { ScrollAnchorProvider } from '@providers/scroll-anchor';
+import { Inter } from 'next/font/google';
+import { Metadata } from 'next/types';
+import React from 'react';
 
 export const metadata: Metadata = {
     description: 'Inspect transactions, accounts, blocks, and more on the RENEC blockchain',
@@ -31,32 +32,32 @@ const rubikFont = Inter({
 });
 
 export default function RootLayout({
-                                       analytics,
-                                       children,
-                                   }: {
+    analytics,
+    children,
+}: {
     analytics?: React.ReactNode;
     children: React.ReactNode;
 }) {
     return (
         <html lang="en" className={`${rubikFont.variable}`}>
-        <body>
-        <ScrollAnchorProvider>
-            <ClusterProvider>
-                <ClusterModal/>
-                <div className="main-content d-flex flex-column">
-                    <Navbar/>
-                    <MessageBanner/>
-                    <ClusterStatusBanner/>
-                    <SearchBar/>
-                    <div className="main-body">
-                        {children}
-                    </div>
-                    <Footer/>
-                </div>
-            </ClusterProvider>
-        </ScrollAnchorProvider>
-        {analytics}
-        </body>
+            <body>
+                <ScrollAnchorProvider>
+                    <ClusterProvider>
+                        <LanguageProvider>
+                            <ClusterModal />
+                            <div className="main-content d-flex flex-column">
+                                <Navbar />
+                                <MessageBanner />
+                                <ClusterStatusBanner />
+                                <SearchBar />
+                                <div className="main-body">{children}</div>
+                                <Footer />
+                            </div>
+                        </LanguageProvider>
+                    </ClusterProvider>
+                </ScrollAnchorProvider>
+                {analytics}
+            </body>
         </html>
     );
 }
---
+++ app/locales/en.ts
@@ -0,0 +1,9 @@
+const localeEn: { [key: string]: any } = {
+    blocks: 'Blocks',
+    dashboard: 'Dashboard',
+    supply: 'Supply',
+    transactions: 'Transactions',
+    validators: 'Validators',
+};
+
+export default localeEn;
