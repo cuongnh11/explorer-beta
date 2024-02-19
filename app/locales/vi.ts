ee6865af16f9f8e14e464deda19da74770d8a7e1
Pham Anh Tuan <tuanpham@remitano.com>
2024-02-19 16:42:50 +0700

    [renec] setup multiple language for explore beta

---
+++ app/locales/vi.ts
@@ -0,0 +1,9 @@
+const localeVi: { [key: string]: any } = {
+    blocks: 'Chuỗi khối',
+    dashboard: 'Bảng Điều Khiển',
+    supply: 'Nguồn Cung',
+    transactions: 'Giao dịch',
+    validators: 'Người xác thực',
+};
+
+export default localeVi;
