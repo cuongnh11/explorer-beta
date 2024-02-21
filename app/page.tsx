7858afdc7f1d399ffbf7833b42e683c303bda331
Cuong Nguyen Huy <97592478+cuongnh11@users.noreply.github.com>
2024-02-21 09:59:35 +0700

    Revert "[ready][renec] setup multiple language for explore beta"

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,23 @@
 'use client';
 
-import { ErrorCard } from '@components/common/ErrorCard';
-import { LoadingCard } from '@components/common/LoadingCard';
-import { LiveTransactionStatsCard } from '@components/LiveTransactionStatsCard';
-import { RecentTransactionsCard } from '@components/RecentTransactions';
-import { StatsNotReady } from '@components/StatsNotReady';
-import { useVoteAccounts } from '@providers/accounts/vote-accounts';
-import { useCluster } from '@providers/cluster';
-import { useLanguage } from '@providers/language-provider';
-import { StatsProvider } from '@providers/stats';
+import {ErrorCard} from '@components/common/ErrorCard';
+import {LoadingCard} from '@components/common/LoadingCard';
+import {LiveTransactionStatsCard} from '@components/LiveTransactionStatsCard';
+import {RecentTransactionsCard} from "@components/RecentTransactions";
+import {StatsNotReady} from '@components/StatsNotReady';
+import {useVoteAccounts} from '@providers/accounts/vote-accounts';
+import {useCluster} from '@providers/cluster';
+import {StatsProvider} from '@providers/stats';
 import {
-    ClusterStatsStatus,
-    useDashboardInfo,
-    usePerformanceInfo,
-    useStatsProvider,
+  ClusterStatsStatus,
+  useDashboardInfo,
+  usePerformanceInfo,
+  useStatsProvider,
 } from '@providers/stats/solanaClusterStats';
-import { Status, SupplyProvider, useFetchSupply, useSupply } from '@providers/supply';
-import { ClusterStatus } from '@utils/cluster';
-import { abbreviatedNumber, lamportsToSol, slotsToHumanString } from '@utils/index';
-import { percentage } from '@utils/math';
+import {Status, SupplyProvider, useFetchSupply, useSupply} from '@providers/supply';
+import {ClusterStatus} from '@utils/cluster';
+import {abbreviatedNumber, lamportsToSol, slotsToHumanString} from '@utils/index';
+import {percentage} from '@utils/math';
 import React from 'react';
 
 export default function Page() {
@@ -26,10 +25,10 @@ export default function Page() {
         <StatsProvider>
             <SupplyProvider>
                 <div className="container mt-4">
-                    <StatsCardBody />
-                    <LiveTransactionStatsCard />
-                    <StakingComponent />
-                    <RecentTransactionsCard />
+                    <StatsCardBody/>
+                    <LiveTransactionStatsCard/>
+                    <StakingComponent/>
+                    <RecentTransactionsCard/>
                 </div>
             </SupplyProvider>
         </StatsProvider>
@@ -37,11 +36,10 @@ export default function Page() {
 }
 
 function StakingComponent() {
-    const { t } = useLanguage();
-    const { status } = useCluster();
+    const {status} = useCluster();
     const supply = useSupply();
     const fetchSupply = useFetchSupply();
-    const { fetchVoteAccounts, voteAccounts } = useVoteAccounts();
+    const {fetchVoteAccounts, voteAccounts} = useVoteAccounts();
 
     function fetchData() {
         fetchSupply();
@@ -62,10 +60,7 @@ function StakingComponent() {
 
     const activeStake = React.useMemo(() => {
         if (voteAccounts && delinquentStake) {
-            return (
-                voteAccounts.current.reduce((prev, current) => prev + current.activatedStake, BigInt(0)) +
-                delinquentStake
-            );
+            return voteAccounts.current.reduce((prev, current) => prev + current.activatedStake, BigInt(0)) + delinquentStake;
         }
     }, [voteAccounts, delinquentStake]);
 
@@ -75,9 +70,9 @@ function StakingComponent() {
     }
 
     if (supply === Status.Idle || supply === Status.Connecting) {
-        return <LoadingCard message={t('loading_supply_and_price_data')} />;
+        return <LoadingCard message="Loading supply data"/>;
     } else if (typeof supply === 'string') {
-        return <ErrorCard text={supply} retry={fetchData} />;
+        return <ErrorCard text={supply} retry={fetchData}/>;
     }
 
     // Calculate to 2dp for accuracy, then display as 1
@@ -93,7 +88,7 @@ function StakingComponent() {
             <div className="col-12 col-xl-6">
                 <div className="card">
                     <div className="card-body">
-                        <h4>{t('active_stake')}</h4>
+                        <h4>Active Stake</h4>
                         {activeStake ? (
                             <h1>
                                 <em>{displayLamports(activeStake)}</em> / <small>{displayLamports(supply.total)}</small>
@@ -101,7 +96,7 @@ function StakingComponent() {
                         ) : null}
                         {delinquentStakePercentage && (
                             <h5>
-                                {t('delinquent_stake')}: <em>{delinquentStakePercentage}%</em>
+                                Delinquent stake: <em>{delinquentStakePercentage}%</em>
                             </h5>
                         )}
                     </div>
@@ -110,13 +105,13 @@ function StakingComponent() {
             <div className="col-12 col-xl-6">
                 <div className="card">
                     <div className="card-body">
-                        <h4>{t('circulating_supply')}</h4>
+                        <h4>Circulating Supply</h4>
                         <h1>
                             <em>{displayLamports(supply.circulating)}</em> /{' '}
                             <small>{displayLamports(supply.total)}</small>
                         </h1>
                         <h5>
-                            <em>{circulatingPercentage}%</em> {t('percentage_is_circulating')}
+                            <em>{circulatingPercentage}%</em> is circulating
                         </h5>
                     </div>
                 </div>
@@ -130,11 +125,10 @@ function displayLamports(value: number | bigint) {
 }
 
 function StatsCardBody() {
-    const { t } = useLanguage();
     const dashboardInfo = useDashboardInfo();
     const performanceInfo = usePerformanceInfo();
-    const { setActive } = useStatsProvider();
-    const { cluster } = useCluster();
+    const {setActive} = useStatsProvider();
+    const {cluster} = useCluster();
 
     React.useEffect(() => {
         setActive(true);
@@ -142,36 +136,40 @@ function StatsCardBody() {
     }, [setActive, cluster]);
 
     if (performanceInfo.status === ClusterStatsStatus.Loading) {
-        return <LoadingCard message={t('loading_epoch_data')} />;
+        return <LoadingCard message="Loading epoch data"/>
     }
 
     if (performanceInfo.status !== ClusterStatsStatus.Ready || dashboardInfo.status !== ClusterStatsStatus.Ready) {
         const error =
             performanceInfo.status === ClusterStatsStatus.Error || dashboardInfo.status === ClusterStatsStatus.Error;
-        return <StatsNotReady error={error} />;
+        return <StatsNotReady error={error}/>;
     }
 
-    const { avgSlotTime_1h, avgSlotTime_1min, epochInfo } = dashboardInfo;
+    const {avgSlotTime_1h, avgSlotTime_1min, epochInfo} = dashboardInfo;
     const hourlySlotTime = Math.round(1000 * avgSlotTime_1h);
     const averageSlotTime = Math.round(1000 * avgSlotTime_1min);
-    const { slotIndex, slotsInEpoch } = epochInfo;
+    const {slotIndex, slotsInEpoch} = epochInfo;
     const epochProgress = percentage(slotIndex, slotsInEpoch, 2).toFixed(1) + '%';
     const epochTimeRemaining = slotsToHumanString(Number(slotsInEpoch - slotIndex), hourlySlotTime);
-    const { absoluteSlot } = epochInfo;
+    const {absoluteSlot} = epochInfo;
 
     return (
         <div className="card epoch-stats">
             <div className="row card-body">
                 <div className="col-12 col-xl-3">
-                    <div className="mb-2 mb-xl-3">{t('slot_height')}</div>
-                    <h1 className="text-primary pb-0 mb-0">{absoluteSlot.toLocaleString()}</h1>
+                    <div className="mb-2 mb-xl-3">Slot height</div>
+                    <h1 className="text-primary pb-0 mb-0">
+                        {absoluteSlot.toLocaleString()}
+                    </h1>
                 </div>
                 <div className="col-12 col-xl-3 mt-4 mt-xl-0">
-                    <div className="mb-2 mb-xl-3">{t('current_slot_time')}</div>
-                    <h1 className="text-primary pb-0 mb-0">{averageSlotTime} ms</h1>
+                    <div className="mb-2 mb-xl-3">Current slot time</div>
+                    <h1 className="text-primary pb-0 mb-0">
+                        {averageSlotTime} ms
+                    </h1>
                 </div>
                 <div className="col-12 col-xl-6 mt-4 mt-xl-0">
-                    <div className="mb-2 mb-xl-3">{t('epoch')}</div>
+                    <div className="mb-2 mb-xl-3">Epoch</div>
                     <div className="d-flex align-items-center">
                         <h1 className="text-primary pb-0 mb-0">{epochInfo.epoch.toLocaleString('en-US')}</h1>
                         <div className="flex-grow-1 mx-5">
@@ -185,7 +183,7 @@ function StatsCardBody() {
                                     role="progressbar"
                                     aria-valuemin={0}
                                     aria-valuemax={100}
-                                    style={{ width: epochProgress }}
+                                    style={{width: epochProgress}}
                                 />
                             </div>
                         </div>
